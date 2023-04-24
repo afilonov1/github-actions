4a5fb35adc3d7c39545e12246111c8379a408f67
filonov <filonov@lk-soft.ru>
2023-04-24 16:50:20 +0300

    fixed tests

--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -5,11 +5,15 @@ public class TestCalculator {
     Calculator calculator = new Calculator();
     @Test
     public void testAdd() {
-        assertEquals(calculator.add(1.0 , 2.0), 1);
+        assertEquals(calculator.add(1.0 , 0.0), 1);
     }
 
     @Test
     public void testSub() {
         assertEquals(calculator.sub(1.0 , 2.0), -1);
+    }
+    @Test
+    public void testDiv() {
+        assertEquals(calculator.div(1.0 , 2.0), 0.5);
     }
 }
